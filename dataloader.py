4a85dbd8ed6eebc7efcb69385144eda394638ba0
Will Sutton <wsutton17@gmail.com>
2019-07-07 23:53:28 -0400

    file typo protocol

--- dataloader.py
+++ dataloader.py
@@ -208,7 +208,7 @@ class DataLoader:
         print('testing2 ftp')
 
     def hello_world(self):
-        print('ftp works2')
+        print('the real test')
     
     def get_all_sirna_df(self, sirna=None):
         '''return the train rows with a certain sirna'''
